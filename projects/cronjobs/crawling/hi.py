49a5162d3f373056bcce2241dac6eb84a5f3c186
jujumilk3 <jujumilk3@gmail.com>
2024-02-17 15:08:46 +0900

    init cronjobs project

---
+++ projects/cronjobs/crawling/hi.py
@@ -0,0 +1,6 @@
+from shared.constants.enums import BlogParentPlatforms
+
+if __name__ == "__main__":
+    print(BlogParentPlatforms)
+    print(len(BlogParentPlatforms))
+    assert len(BlogParentPlatforms) > 0
